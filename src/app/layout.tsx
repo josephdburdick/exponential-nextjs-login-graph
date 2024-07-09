8629843ebc10c79c896f4742bbd3d3f6b3b5248f
Joe <josephdburdick@gmail.com>
2024-07-08 21:43:13 -0400

    Add prettier/eslint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={cn("min-h-screen bg-background font-sans antialiased")}>
-        {children}
-      </body>
+      <body className={cn("min-h-screen bg-background font-sans antialiased")}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import { Button, buttonVariants } from "@/components/ui/button";
-import ROUTES from "@/lib/constants/routes";
-import { cn } from "@/lib/utils";
-import Link from "next/link";
+import { Button, buttonVariants } from '@/components/ui/button'
+import ROUTES from '@/lib/constants/routes'
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex min-h-[100dvh] flex-col items-center justify-center bg-background px-4 py-12 sm:px-6 lg:px-8">
-        <div className="mx-auto max-w-md text-center space-y-8">
+        <div className="mx-auto max-w-md space-y-8 text-center">
           <div className="space-y-4">
             <h1 className="text-4xl font-bold tracking-tight text-foreground sm:text-5xl">
               Coming Soon
@@ -20,12 +20,12 @@ export default function Home() {
 
           <Link
             href={ROUTES.login.path}
-            className={cn(buttonVariants({ variant: "outline" }))}
+            className={cn(buttonVariants({ variant: 'outline' }))}
           >
             {ROUTES.login.label}
           </Link>
         </div>
       </div>
     </main>
-  );
+  )
 }
