2aee8666cf7a854e20ffe797c7eb550ee3226219
Joe <josephdburdick@gmail.com>
2024-07-08 22:11:05 -0400

    Fix nagging config

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').options} */
 const config = {
-  plugins: ['prettier-plugin-tailwindcss'],
+  plugins: ["prettier-plugin-tailwindcss"],
   semi: false,
   singleQuote: true,
-}
+};
 
-export default config
+export default config;
