774290fde00bec43964d1f164615099452661c56
Joe <josephdburdick@gmail.com>
2024-07-08 22:25:14 -0400

    Protect user/pass inside .env.local

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -3,8 +3,29 @@ import { NextRequest, NextResponse } from 'next/server'
 export async function POST(request: NextRequest) {
   const { username, password } = await request.json()
 
-  return NextResponse.json({
-    username,
-    password,
-  })
+  if (
+    username === process.env.USER_NAME &&
+    password === process.env.USER_PASS
+  ) {
+    const response = NextResponse.json({
+      id: 5301,
+      username: 'test user',
+      contracts: [
+        {
+          id: 301,
+          symbol: 'eth_lido',
+          holding: 325.1,
+        },
+      ],
+    })
+
+    response.cookies.set('token', 'exampletoken', {
+      httpOnly: true,
+      path: '/',
+    })
+
+    return response
+  } else {
+    return NextResponse.json({ error: 'Invalid credentials' }, { status: 401 })
+  }
 }
