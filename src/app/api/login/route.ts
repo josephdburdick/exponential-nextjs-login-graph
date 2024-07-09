766f0b5da2129aca418e46f02735c32ad42a7cf3
Joe <josephdburdick@gmail.com>
2024-07-08 20:40:00 -0400

    POST request returns sent JSON data

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,5 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
-  return NextResponse.json({ hello: "world" });
+  const { email, password } = await request.json();
+  return NextResponse.json({
+    email,
+    password,
+  });
 }
